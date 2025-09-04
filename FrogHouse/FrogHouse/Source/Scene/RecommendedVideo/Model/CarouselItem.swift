e3b62cdf5813f802e1018f8d5f3bd01fe3bef6cc
jaypark <dearjaypark@gmail.com>
2025-09-04 18:10:23 +0900

    [FEAT] CarouseItem 과 DummyData 추가

---
+++ FrogHouse/FrogHouse/Source/Scene/RecommendedVideo/Model/CarouselItem.swift
@@ -0,0 +1,16 @@
+//
+//  CarouselItem.swift
+//  FrogHouse
+//
+//  Created by JAY on 9/4/25.
+//
+
+import Foundation
+
+
+struct CarouselItem: Hashable {
+  let imageName: String
+  let title: String
+  let detail: String
+  let tags: [String]
+}
