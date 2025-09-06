9a65faf06584a4ec9472967fe69916c8992c5b30
seo-garden <indian4589@naver.com>
2025-09-06 15:13:14 +0900

    [FEAT] RegionCode 및 VideoCategory 모델 파일 추가

---
+++ FrogHouse/FrogHouse/Source/Common/Models/RegionCode.swift
@@ -0,0 +1,14 @@
+//
+//  RegionCode.swift
+//  FrogHouse
+//
+//  Created by 서정원 on 9/6/25.
+//
+
+import Foundation
+
+enum RegionCode: String, Encodable {
+    case korea = "KR"
+    case japan = "JP"
+    case usa = "US"
+}
---
+++ FrogHouse/FrogHouse/Source/Common/Models/VideoCategory.swift
@@ -0,0 +1,16 @@
+//
+//  VideoCategory.swift
+//  FrogHouse
+//
+//  Created by 서정원 on 9/6/25.
+//
+
+import Foundation
+
+enum VideoCategory: String, Encodable {
+    case music = "10"
+    case sports = "17"
+    case travel = "19"
+    case gaming = "20"
+    case movies = "30"
+}
